1ed658700b1a91353be95569deb8b4d4b5223991
JulieMontoux <julie.montoux@epsi.fr>
2024-02-20 17:36:57 +0100

    etape 3

--- main.py
+++ main.py
@@ -1,27 +1,38 @@
+import os
 import requests
 from bs4 import BeautifulSoup
 import csv
 import time
+from urllib.parse import urljoin
+
+def scrape_categories(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        categories_container = soup.find('div', class_='side_categories')
+        categories_list = categories_container.find('ul').find_all('a')
+        category_urls = [urljoin(url, category['href']) for category in categories_list]
+        return category_urls
+    else:
+        print("Failed to retrieve categories from:", url)
+        return []
 
 def scrape_book_details(url):
     response = requests.get(url)
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
-        books = soup.find_all('h3')  # Extracting all book links
+        books = soup.find_all('h3')
         
-        # List to store book details
         all_books_data = []
         
         for book in books:
             book_url = book.find('a')['href']
-            book_full_url = url.rsplit('/', 1)[0] + '/' + book_url
+            book_full_url = urljoin(url, book_url)
             
-            # Scraping individual book details
             book_data = scrape_book_info(book_full_url)
             if book_data:
                 all_books_data.append(book_data)
             
-            # Delaying between requests to avoid overloading the server
             time.sleep(1)
         
         return all_books_data
@@ -34,7 +45,6 @@ def scrape_book_info(url):
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
         
-        # Extracting required information
         product_page_url = url
         title = soup.find('h1').text.strip()
         upc = soup.select_one('th:-soup-contains("UPC") + td').text.strip()
@@ -46,7 +56,8 @@ def scrape_book_info(url):
         review_rating = soup.find('p', class_='star-rating')['class'][1]
         image_url = soup.find('img')['src']
 
-        # récupère le stock
+        image_url = urljoin(url, image_url)
+        
         start_index = availability.find('(')
         end_index = availability.find(' available)')
         if start_index != -1 and end_index != -1:
@@ -70,7 +81,12 @@ def scrape_book_info(url):
         print("Failed to retrieve page:", url)
         return None
 
-def write_to_csv(data, filename):
+def write_to_csv(data, category_name):
+    directory = 'scraped_books'
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    
+    filename = f'{directory}/{category_name}_books.csv'
     with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
         fieldnames = data[0].keys()
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
@@ -80,20 +96,12 @@ def write_to_csv(data, filename):
             writer.writerow(item)
 
 if __name__ == "__main__":
-    # URL de la catégorie de livre à scraper
-    category_url = "http://books.toscrape.com/catalogue/category/books/mystery_3/index.html"
-    
-    # Scraping book details for all pages in the category
-    all_books_data = []
-    page_num = 1
-    while True:
-        url = category_url if page_num == 1 else category_url.replace('index.html', f'page-{page_num}.html')
-        page_books_data = scrape_book_details(url)
-        if not page_books_data:
-            break
-        all_books_data.extend(page_books_data)
-        page_num += 1
-    
-    # Writing scraped data to CSV
-    write_to_csv(all_books_data, 'scraped_books_category.csv')
-    print("Scraping completed successfully.")
+    base_url = "http://books.toscrape.com/catalogue/category/books_1/index.html"
+    category_urls = scrape_categories(base_url)
+
+    for category_url in category_urls:
+        category_name = category_url.split('/')[-2]
+        books_data = scrape_book_details(category_url)
+        if books_data:
+            write_to_csv(books_data, category_name)
+            print(f"Scraping completed successfully for category: {category_name}.")
